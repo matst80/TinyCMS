e6c250a745a396163c5086fe171df31b47af5a21
Mats Törnberg <mats.tornberg@gmail.com>
2018-11-13 08:01:41 +0100

    updated unsecure package

--- TinyCMS/Startup.cs
+++ TinyCMS/Startup.cs
@@ -88,16 +88,11 @@ namespace TinyCMS
             {
                 app.UseExceptionHandler("/Error");
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
-            app.UseHttpsRedirection();
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "TinyCMS API V1"); });
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
