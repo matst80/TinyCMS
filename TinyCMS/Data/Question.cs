70267e266cd07572fcb4331654ae1b52e097608b
Mats Törnberg <mats.tornberg@gmail.com>
2018-11-20 08:53:13 +0100

    questions data models

---
+++ TinyCMS/Data/Question.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using TinyCMS.Data;
+
+namespace TinyCMS.QuestionNodes
+{
+    [Serializable]
+    public class QuestionAnswer
+    {
+        public string Val { get; set; }
+        public int Points { get; set; }
+    }
+
+    [Serializable]
+    public class QuestionCategory : BaseNode
+    {
+        public override string Type => "form-category";
+
+        public string Title { get; set; }
+    }
+
+    [Serializable]
+    public class Question : BaseNode
+    {
+        public override string Type => "question";
+
+        public string Title { get; set; }
+
+        public List<QuestionAnswer> Answers { get; set; } = new List<QuestionAnswer>();
+    }
+
+    [Serializable]
+    public class QuestionMultiple : BaseNode
+    {
+        public override string Type => "question-multiple";
+
+        public string Title { get; set; }
+        public List<QuestionAnswer> Answers { get; set; } = new List<QuestionAnswer>();
+    }
+}
