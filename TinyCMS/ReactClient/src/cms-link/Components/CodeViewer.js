5b7c376e60f8fdc5d41beeb5fe332263ee8320dd
Mats Törnberg <mats.tornberg@gmail.com>
2018-11-30 10:53:13 +0100

    reverted to working application

--- TinyCMS/ReactClient/src/cms-link/Components/CodeViewer.js
+++ TinyCMS/ReactClient/src/cms-link/Components/CodeViewer.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { base16AteliersulphurpoolLight } from 'react-syntax-highlighter/dist/styles/prism';
+//import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+//import { base16AteliersulphurpoolLight } from 'react-syntax-highlighter/dist/styles/prism';
 import { createLinkWrapper } from '../createLinkWrapper';
 
 const CodeViewer = ({ code, codeLang }) => (
     <div>
-        <SyntaxHighlighter language={codeLang} style={base16AteliersulphurpoolLight}>{code}</SyntaxHighlighter>
+        {/* <SyntaxHighlighter language={codeLang}>{code}</SyntaxHighlighter> */}
+        <pre>{code}</pre>
     </div>
 );
 
